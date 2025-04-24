edfcc1a3b622d5ea26f95a04f9a62fccba310efe
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-04-24 16:26:15 +0900

    add minimum functions

---
+++ fw-sample.go
@@ -0,0 +1,76 @@
+package fwsample
+
+import (
+	"errors"
+	"net/http"
+)
+
+// アプリケーション全体を管理する構造体
+type App struct {
+	Router *Router
+}
+
+// ルーティング処理を担当する構造体
+type Router struct {
+	routingTable map[string]map[string]func(http.ResponseWriter, *http.Request)
+}
+
+// ルーティングテーブルを初期化する関数
+func (r *Router) add(method, path string, handler func(http.ResponseWriter, *http.Request)) error {
+	if r.routingTable[method] == nil {
+		r.routingTable[method] = make(map[string]func(http.ResponseWriter, *http.Request))
+	}
+	if r.routingTable[method][path] != nil {
+		return errors.New("handler already exists")
+	}
+	r.routingTable[method][path] = handler
+	return nil
+}
+
+func (r *Router) Add(method, path string, handler func(http.ResponseWriter, *http.Request)) error {
+	if method != http.MethodGet && method != http.MethodPost {
+		return errors.New("unsupported method")
+	}
+	return r.add(method, path, handler)
+}
+
+func (r *Router) Get(path string, handler func(http.ResponseWriter, *http.Request)) error {
+	return r.Add(http.MethodGet, path, handler)
+}
+
+func (r *Router) Post(path string, handler func(http.ResponseWriter, *http.Request)) error {
+	return r.Add(http.MethodGet, path, handler)
+}
+
+
+// ServeHTTPメソッドを実装することで、http.Handlerインターフェースを満たす
+// これにより、http.ListenAndServeに渡すことができるようになる
+func (h *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			if handler, ok := h.Router.routingTable[r.Method][r.URL.Path]; ok {
+				handler(w, r)
+				return
+			}
+		} else if r.Method == http.MethodPost {
+			if handler, ok := h.Router.routingTable[r.Method][r.URL.Path]; ok {
+				handler(w, r)
+				return
+			}
+		}
+		http.NotFound(w, r)
+	})
+	handler.ServeHTTP(w, r)
+}
+
+func New() *App {
+	return &App{
+		Router: &Router{
+			routingTable: make(map[string]map[string]func(http.ResponseWriter, *http.Request)),
+		},
+	}
+}
+
+func (e *App) Run(addr string) error {
+	return http.ListenAndServe(addr, e)
+}
