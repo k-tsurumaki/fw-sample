4d23c55ef4d0cddd6c7a1884ca9e2dc9183c994b
k-tsurumaki <k-tsurumaki@nri.co.jp>
2025-04-25 10:31:20 +0900

    fix comments

--- fw-sample.go
+++ fw-sample.go
@@ -15,7 +15,7 @@ type Router struct {
 	routingTable map[string]map[string]func(http.ResponseWriter, *http.Request)
 }
 
-// ルーティングテーブルを初期化する関数
+// ルーティングテーブルにハンドラを追加する関数
 func (r *Router) add(method, path string, handler func(http.ResponseWriter, *http.Request)) error {
 	if r.routingTable[method] == nil {
 		r.routingTable[method] = make(map[string]func(http.ResponseWriter, *http.Request))
@@ -34,35 +34,34 @@ func (r *Router) Add(method, path string, handler func(http.ResponseWriter, *htt
 	return r.add(method, path, handler)
 }
 
+
+// GETメソッドのハンドラを追加する関数
 func (r *Router) Get(path string, handler func(http.ResponseWriter, *http.Request)) error {
 	return r.Add(http.MethodGet, path, handler)
 }
 
+// POSTメソッドのハンドラを追加する関数
 func (r *Router) Post(path string, handler func(http.ResponseWriter, *http.Request)) error {
 	return r.Add(http.MethodPost, path, handler)
 }
 
-
-// ServeHTTPメソッドを実装することで、http.Handlerインターフェースを満たす
+// ServeHTTPメソッドを実装することで、http.Handlerインターフェースを満たすようになる
 // これにより、http.ListenAndServeに渡すことができるようになる
 func (h *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			if handler, ok := h.Router.routingTable[r.Method][r.URL.Path]; ok {
-				handler(w, r)
-				return
-			}
-		} else if r.Method == http.MethodPost {
-			if handler, ok := h.Router.routingTable[r.Method][r.URL.Path]; ok {
-				handler(w, r)
-				return
-			}
-		}
+        if handlers, ok := h.Router.routingTable[r.Method]; ok {
+            if handler, ok := handlers[r.URL.Path]; ok {
+                handler(w, r)
+                return
+            }
+        }
+		// ルーティングにマッチしなかった場合は404エラーを返す
 		http.NotFound(w, r)
 	})
 	handler.ServeHTTP(w, r)
 }
 
+// App構造体の新しいインスタンスを作成し、初期化する関数
 func New() *App {
 	return &App{
 		Router: &Router{
@@ -71,6 +70,7 @@ func New() *App {
 	}
 }
 
+// アプリケーションを指定したアドレスで起動する関数
 func (e *App) Run(addr string) error {
 	return http.ListenAndServe(addr, e)
 }
